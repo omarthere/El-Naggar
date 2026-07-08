1e8ac9dc4bdef5a5f4267ccac7a78e6f76a82bea
Omar Mohammed <kindofme564@gmail.com>
2026-07-08 09:51:52 +0300

    Update layout by removing Toaster and changing icon
    Removed Toaster component and updated icon path.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +27,7 @@ export const metadata: Metadata = {
     "Egypt",
   ],
   icons: {
-    icon: "/images/carousel_1.png",
+    icon: "/images/logo.png",
   },
   openGraph: {
     title: "EL NAGGAR CONTRACTING | You Dream..We Build",
@@ -49,7 +48,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
         {children}
-        <Toaster />
       </body>
     </html>
   );
